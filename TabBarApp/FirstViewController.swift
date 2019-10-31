c3c4f042c408c22f724a65a514d05851a1679797
Maxim <butin.m@ext-system.com>
2019-10-31 17:07:52 +0700

    Initial Commit

---
+++ TabBarApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabBarApp
+//
+//  Created by Максим Бутин on 31.10.2019.
+//  Copyright © 2019 Максим Бутин. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabBarApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabBarApp
+//
+//  Created by Максим Бутин on 31.10.2019.
+//  Copyright © 2019 Максим Бутин. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
